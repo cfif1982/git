f77a76a8125619f46681f67fc6d1adb43606e7aa
cfif <s_filip@mail.ru>
2024-11-14 15:30:51 +0400

    v01

--- main.go
+++ main.go
@@ -2,11 +2,8 @@ package main
 
 import (
 	"fmt"
-	"training/internal/workerpool" // путь к вашему пакету examples
 )
 
 func main() {
-	// Вызов примера MainWorkerpool
-	fmt.Println("=== MainWorkerpool() ===")
-	workerpool.MainWorkerpool()
+	fmt.Println("git v 01")
 }
